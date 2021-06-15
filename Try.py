69c7ae5df1a4a93c4abdcc798de971fce25a5191
GamerTyron78 <85962957+GamerTyron78@users.noreply.github.com>
2021-06-15 22:47:15 +0300

    Create Try.py

---
+++ Try.py
@@ -0,0 +1,279 @@
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+101
+102
+103
+104
+105
+106
+107
+108
+109
+110
+111
+112
+113
+114
+115
+116
+117
+118
+119
+120
+121
+122
+123
+124
+125
+126
+127
+128
+129
+130
+131
+132
+133
+134
+135
+136
+137
+138
+139
+import random
+import turtle
+import time
+delay = 0.15
+ 
+pencere = turtle.Screen()
+pencere.title('Yılan Oyunu')
+pencere.bgcolor('lightgreen')
+pencere.setup(width=600, height=600)
+pencere.tracer(0)
+ 
+kafa = turtle.Turtle()
+kafa.speed(0)
+kafa.shape("square")
+kafa.color("black")
+kafa.penup()
+kafa.goto(0, 100)
+kafa.direction = "stop"
+ 
+yemek = turtle.Turtle()
+yemek.speed(0)
+yemek.shape("circle")
+yemek.color("red")
+yemek.penup()
+yemek.shapesize(0.80, 0.80)
+yemek.goto(0, 0)
+ 
+kuyruklar = []
+puan = 0
+ 
+yaz = turtle.Turtle()
+yaz.speed(0)
+yaz.shape("square")
+yaz.color("white")
+yaz.penup()
+yaz.hideturtle()
+yaz.goto(0, 260)
+yaz.write("Puan: {}".format(puan), align="center", font=("Courier", 24, "normal"))
+ 
+def move():
+    if kafa.direction == "up":
+        y = kafa.ycor()
+        kafa.sety(y + 20)
+    if kafa.direction == "down":
+        y = kafa.ycor()
+        kafa.sety(y - 20)
+    if kafa.direction == "right":
+        x = kafa.xcor()
+        kafa.setx(x + 20)
+    if kafa.direction == "left":
+        x = kafa.xcor()
+        kafa.setx(x - 20)
+ 
+def go_up():
+    if kafa.direction != "down":
+        kafa.direction = "up"
+def go_down():
+    if kafa.direction != "up":
+        kafa.direction = "down"
+ 
+ 
+ 
+ 
+ 
+def go_right():
+    if kafa.direction != "left":
+        kafa.direction = "right"
+def go_left():
+    if kafa.direction != "right":
+        kafa.direction = "left"
+ 
+pencere.listen()
+pencere.onkey(go_up, "Up")
+pencere.onkey(go_down, "Down")
+pencere.onkey(go_right, "Right")
+pencere.onkey(go_left, "Left")
+ 
+while True:
+    pencere.update()
+ 
+    if kafa.xcor() > 300 or kafa.xcor() < -300 or kafa.ycor() > 300 or kafa.ycor() < -300:
+        time.sleep(1)
+        kafa.goto(0, 0)
+        kafa.direction = "stop"
+ 
+        for kuyruk in kuyruklar:
+            kuyruk.goto(1000, 1000)
+        kuyruklar = []
+ 
+        puan = 0
+        delay = 0.1
+ 
+        yaz.clear()
+        yaz.write("Puan: {}".format(puan), align="center", font=("Courier", 24, "normal"))
+ 
+    if kafa.distance(yemek) < 20:
+        x = random.randint(-250, 250)
+        y = random.randint(-250, 250)
+        yemek.goto(x, y)
+ 
+        yeni_kuyruk = turtle.Turtle()
+        yeni_kuyruk.speed(0)
+        yeni_kuyruk.shape("square")
+        yeni_kuyruk.color("white")
+        yeni_kuyruk.penup()
+        kuyruklar.append(yeni_kuyruk)
+ 
+        delay -= 0.001
+ 
+        puan = puan + 10
+        yaz.clear()
+        yaz.write("Puan: {}".format(puan), align="center", font=("Courier", 24, "normal"))
+ 
+    for index in range(len(kuyruklar) - 1, 0, -1):
+        x = kuyruklar[index - 1].xcor()
+        y = kuyruklar[index - 1].ycor()
+        kuyruklar[index].goto(x, y)
+ 
+    if len(kuyruklar) > 0:
+        x = kafa.xcor()
+        y = kafa.ycor()
+        kuyruklar[0].goto(x, y)
+ 
+    move()
+ 
+    for segment in kuyruklar:
+        if segment.distance(kafa) < 20:
+            time.sleep(1)
+            kafa.goto(0, 0)
+            kafa.direction = "stop"
+            for segment in kuyruklar:
+                segment.goto(1000, 1000)
+            kuyruklar = []
+            puan = 0
+            yaz.clear()
+            yaz.write('Puan: {}'.format(puan), align='center', font=('Courier', 24, 'normal'))
+            hiz = 0.15
+ 
+    time.sleep(delay)
